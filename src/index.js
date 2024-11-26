f371a8aff913372a94bac35fc44c8680b2fb8ce8
Clive Kimari <kimariclive@gmail>
2024-11-26 22:29:19 +0000

    widening the margin of padding

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // Ensure the correct file path to App.js
+import "normalize.css";
 import { GlobalStyles } from "./global-styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
