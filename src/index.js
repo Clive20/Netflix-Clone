2b5bc21083685215eae2b0412df056270249bb06
Clive Kimari <kimariclive@gmail>
2024-11-26 22:21:49 +0000

    added styled components to the background

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App"; // Ensure the correct file path to App.js
+import { GlobalStyles } from "./global-styles";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+);
